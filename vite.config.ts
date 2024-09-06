fed32b2a53627503194db5d3c7abfe474d9e9445
isaacvr <isaacvega1996@gmail.com>
2024-09-06 19:04:34 -0400

    Actualizar vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -74,7 +74,7 @@ export default defineConfig(env => ({
     // })
   ],
   publicDir: "public",
-  base: env.mode === "production" ? "./" : "",
+  base: env.mode === "production" ? "/" : "",
   server: {
     host: true,
     port: 5432,
