35649e1239ed9778254e8762059e535ff062438e
Isaac Vega Rodríguez <isaacvega1996@gmail.com>
2024-10-22 14:56:27 -0400

    feat(integration): add capacitor for mobile app

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.isaacvr.cubicdb",
+  appName: "CubicDB",
+  webDir: "build",
+  backgroundColor: "#080a16",
+};
+
+export default config;
