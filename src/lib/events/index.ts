bdbcc3b16265fac80b42544cc0fecad3585a56f6
Isaac Vega Rodríguez <isaacvega1996@gmail.com>
2026-07-19 02:43:30 -0400

    feat: export bus-attached event foundation

--- src/lib/events/index.ts
+++ src/lib/events/index.ts
@@ -1,23 +1,25 @@
 // Event system entry point - centralized event bus
-export { TIMER_EVENTS, type TimerEventType } from './timer/TimerEventRegistry';
-export type { TimerEventPayloadMap } from './timer/TimerEventPayloadMap';
-export type { TimerEvent } from './timer/TimerEvent';
+export { TIMER_EVENTS, type TimerEventType } from "./timer/TimerEventRegistry";
+export type { TimerEventPayloadMap } from "./timer/TimerEventPayloadMap";
+export type { TimerEvent } from "./timer/TimerEvent";
 export {
   TimerEventFactory,
   createApplicationEventBus,
   type IEventIdProvider,
   type IMonotonicClock,
   type NativeTimestampSource,
-} from './timer/TimerEventFactory';
-export { EventBus, type IEventBus } from './EventBus';
+} from "./timer/TimerEventFactory";
+export { EventBus, type IEventBus } from "./EventBus";
 export type {
   DeviceLeaseRejectionReason,
   LegacyTimerDeviceDescriptor,
   TimerDeviceDescriptor,
-} from '$lib/timer/devices/TimerDeviceDescriptor';
-export { EventDispatcher } from './EventDispatcher';
-export type { DomainEvent, EventHandler, EventSubscription } from './types';
-export { eventBus } from './singleton';
+} from "$lib/timer/devices/TimerDeviceDescriptor";
+export { EventDispatcher } from "./EventDispatcher";
+export type { DomainEvent, EventHandler, EventSubscription } from "./types";
+export { eventBus } from "./singleton";
 
 // Domain events
-export * from './domain';
+export * from "./domain";
+export * from "./emitters";
+export * from "./modules";
